a99f130e8545a10d371df3ec0e1820d52b41e572
Sergio Álvarez Suárez <sergioalvarezsuarez@gmail.com>
2016-08-30 22:24:32 +0200

    Add watch task and split the main CSS in several files

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,20 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
+var importer = require('postcss-import')
 
-gulp.task('default', function() {
+gulp.task('css', function() {
   var processors = [
+    importer(),
     autoprefixer(),
     cssnano(),
   ];
 
-  return gulp.src('./_css/**/*.css')
+  return gulp.src('./_css/main.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('watch', function() {
+  return gulp.watch('./_css/**/*.css', ['css']);
 });
