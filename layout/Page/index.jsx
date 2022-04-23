de259d0dd82857a78a6a787f0958f3fdeead5ade
Sergio Álvarez <sergio@codecoolture.com>
2022-04-23 19:16:20 +0200

    Fix prop

--- layout/Page/index.jsx
+++ layout/Page/index.jsx
@@ -21,7 +21,7 @@ function Page({ children, theme }) {
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
 
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
 
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
@@ -56,11 +56,11 @@ function Page({ children, theme }) {
         <MDXProvider
           components={{
             a: Link,
-            h1: props => <Heading size="extraLarge" element="h1" {...props} />,
-            h2: props => <Heading size="large" element="h2" {...props} />,
+            h1: (props) => <Heading size="extraLarge" element="h1" {...props} />,
+            h2: (props) => <Heading size="large" element="h2" {...props} />,
             p: Text,
             ul: Bullets,
-            li: Bullet
+            li: Bullet,
           }}
         >
           <section>{children}</section>
